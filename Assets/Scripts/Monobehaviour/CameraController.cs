6141f97ab99422a557b327680e9aa9cdb7944765
Lisranda <xlisranda@gmail.com>
2020-04-23 04:21:47 -0500

    Initial Commit

---
+++ Assets/Scripts/Monobehaviour/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    GameObject player;
+    Quaternion initRotation;
+    Camera cam;
+
+
+    void Awake () {
+        player = transform.parent.gameObject;
+        initRotation = transform.rotation;
+        transform.position = player.transform.position - 15f * transform.forward;
+        cam = GetComponent<Camera> ();
+    }
+
+    void LateUpdate () {
+        FixCamera ();
+    }
+
+    void FixCamera () {
+        transform.rotation = initRotation;
+        transform.position = player.transform.position - 15f * transform.forward;
+    }
+}
---
+++ Assets/Scripts/Monobehaviour/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] LayerMask mouseLookLayer;
+
+    Camera cam;
+    
+    void Awake () {
+        cam = GetComponentInChildren<Camera> ();
+    }
+
+    void Update () {
+        FaceMouse ();
+        Movement ();
+    }
+
+    void FaceMouse () {
+        Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+        if (Physics.Raycast (ray , out RaycastHit hit , 1000f , mouseLookLayer)) {
+            transform.LookAt (hit.point);
+        }
+    }
+
+    void Movement () {
+        Vector3 input = new Vector3 (Input.GetAxisRaw ("Horizontal") , 0 , Input.GetAxisRaw ("Vertical"));
+        transform.Translate (input * 8f * Time.deltaTime);
+    }
+
+}
